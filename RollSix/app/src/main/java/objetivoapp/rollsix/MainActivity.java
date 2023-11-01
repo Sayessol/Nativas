7924fe20d1874ed9736d21d96666f534d5816601
nferron14 <nferron@uoc.edu>
2023-11-01 18:50:12 +0100

    Pantalla de bienvenida 1

--- RollSix/app/src/main/java/objetivoapp/rollsix/MainActivity.java
+++ RollSix/app/src/main/java/objetivoapp/rollsix/MainActivity.java
@@ -8,6 +8,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.Theme_RollSix);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
